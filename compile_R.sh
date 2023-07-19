985bb9a318e7667efa2f4ec7036ce83ca389d49d
Jermiah Joseph <jermiahjoseph98@gmail.com>
2023-07-19 22:24:08 +0000

    fix scripts

---
+++ compile_R.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+# -- make sure we are on an OS with apt
+if [[ ! "$OSTYPE" =~ linux.* ]]
+then
+    echo "This script requires the apt utility to work correctly!"
+    exit 1
+fi
+
+# -- parse command line args
+SOURCE='https://cran.r-project.org/src/base/R-latest.tar.gz'
+VERSION='latest'
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -v|--version)
+            VERSION="$2"
+            case $VERSION in
+                devel)
+                    SOURCE='https://stat.ethz.ch/R/daily/R-devel.tar.gz'z
+                    ;;
+                patched)
+                    SOURCE='https://stat.ethz.ch/R/daily/R-patched.tar.gz'
+                    ;;
+                *)
+                    echo 'Valid options for the -v/--version parameter are ' \
+                        '\n"devel" for the development version or "patched" ' \
+                        '\nfor the previous release. If you want R "release",' \
+                        '\n exclude the -v/--version parameter since that is' \
+                        'the default for this script.'
+                    ;;
+            esac
+            shift # past argument
+            shift # past value
+            ;;
+        *)
+            echo "The only valid option is for this script is -v/--version."
+            exit 1
+            ;;
+    esac
+done
+
+# -- install R compilation dependencies
+## TODO: Generalize this to work with other installation utilties (brew, yum)
+echo "Installing required dependencies"
+sudo apt -y install \
+    build-essential \
+    libcurl4-gnutls-dev \
+    default-jdk \
+    libxml2-dev \
+    libbz2-dev \
+    texlive-full \
+    gfortran \
+    g++ \
+    libreadline-dev \
+    xorg-dev \
+    liblzma-dev \
+    libpcre++-dev \
+    libpango1.0-dev \
+    libpango1.0-dev \
+    libpcre2-dev \
+    tk-dev \
+    tk-table \
+    tcl-dev \
+    zlib1g-dev \
+    libssl-dev
+echo "Dependencies installed!"
+
+# -- download the correct R version
+echo "Downloading R-$VERSION source code..."
+wget $SOURCE
+tar -xvf R-"$VERSION"*tar.gz --one-top-level=R-"$VERSION" --strip-components=1
+echo "Done"
+
+# -- configure and make the R source files
+echo "Switching into R-$VERSION directory..."
+cd "R-$VERSION" || exit 1
+echo "In $(pwd)!"
+echo "Configuring installation..."
+sudo ./configure \
+    --prefix=/home/R/R-"$VERSION" \
+    --enable-memory-profiling \
+    --enable-R-shlib \
+    --with-tcltk \
+    --with-blas \
+    --with-lapack
+echo "Making R-$VERSION..."
+sudo mkdir -m 777 /home/R
+sudo make
+
+# -- install the R source files
+echo "Installing from source..."
+sudo make install
+echo "Done"
+
+# -- update the system symlinks to the version just installed
+echo "Creating symlinks"
+sudo rm /usr/bin/R /usr/bin/Rscript
+sudo ln -s /home/R/R-"$VERSION"/bin/R /usr/bin/R
+sudo ln -s /home/R/R-"$VERSION"/bin/Rscript /usr/bin/Rscript
+echo "Done!"
+
+# -- check if the installation succeeded
+echo "Testing installation was succesful:"
+/home/R/R-"$VERSION"/bin/R --version || exit 1
+echo "Successful if above returned a version number!"
+
+echo "Cleaning up..."
+rm -r R-*
+echo "Finished cleaning up!"
+
+exit 0
--- setup_tools.sh
+++ setup_tools.sh
@@ -1,20 +1,75 @@
 #!/bin/bash
 
-# Update the package lists
-sudo apt update
+# Installing GCSFUSE - tool to mount google cloud buckets
 
-# Install parallel
-sudo apt install -y parallel
+# Add the Cloud Storage FUSE repository
+# Import the Google Cloud public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 
-# Install htop
-sudo apt install -y htop
+# do this before sudo update in order to also update the Google Cloud repo
+export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
 
-# Install s3cmd
-sudo apt install -y python3 python3-pip
-sudo pip3 install s3cmd
+# -- Update and upgrade
+sudo apt-get update -y && sudo apt-get upgrade -y
 
-# Configure s3cmd
-s3cmd --configure
+# -- Install miniconda for current stable Python release
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3*.sh
 
-# Print installation completion message
-echo "Parallel, htop, and s3cmd installed successfully."
+# -- Add R repo to apt, then use it to install R
+sudo apt install dirmngr gnupg apt-transport-https ca-certificates \
+    software-properties-common
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
+    E298A3A825C0D65DFD57CBB651716619E084DAB9
+# Add the CRAN repository to the apt sources list
+echo "deb https://cloud.r-project.org/bin/linux/debian $(lsb_release -cs)-cran40/" | sudo tee /etc/apt/sources.list.d/r.list
+
+
+# -- Install apt useful packages
+#
+apt_pkgs=(
+    # system deps
+    'libcurl4-openssl-dev' 'libssl-dev' 'libxml2-dev' 'build-essential'
+    'libsodium-dev' 'libopenblas-dev' 'gnupg2'
+    # databases
+    'sqlite' 'mariabd-server-10.3' 'mariadb-server' 
+    'libmariadbclient-dev'
+    # CLI tools
+    'git'  'pigz'  'htop' 'thefuck' 'silversearcher-ag' 'parallel' 'tree'
+    # GCP tools
+    'fuse' 'gcsfuse'
+    # programming languages
+    'r-base'
+)
+
+# appends an environment variable export command to the ~/.bashrc file. 
+# This command sets the LD_LIBRARY_PATH variable to /usr/lib/openblas-base/, which is the path to the OpenBLAS library.
+echo "export LD_LIBRARY_PATH=/usr/lib/openblas-base/" >> ~/.bashrc
+
+q
+# iterate over each package in the apt_pkgs array and uses apt-get to install them with the -y flag, 
+# which automatically confirms any prompts that may appear during the installation process. 
+for pkg in "${apt_pkgs[@]}"
+do
+    sudo apt-get install -y "$pkg"
+done
+
+# Install Cloud Storage FUSE
+sudo apt-get install 
+
+
+# # Install s3cmd
+#  commands to instlal and configure the s3 compute canada storage 
+# sudo pip3 install s3cmd
+# # Configure s3cmd
+# s3cmd --configure
+
+
+
+# setup bioconda
+# conda config --add channels defaults
+# conda config --add channels bioconda
+# conda config --add channels conda-forge
+# conda config --set channel_priority strict
