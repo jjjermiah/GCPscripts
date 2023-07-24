cf06f71f335f18facaeca6d64e76e9ec368b29f0
Jermiah Joseph <jermiahjoseph98@gmail.com>
2023-07-24 20:55:37 +0000

    script to install all relevant packages

---
+++ install_pkgs.R
@@ -0,0 +1,26 @@
+#!/bin/R
+
+# Add this to your .Rprofile to permanently set your Bioc version
+Sys.setenv(R_BIOC_VERSION=3.16)
+
+if (!requireNamespace("pak", quietly = TRUE)) {
+    install.packages("pak", repos = "https://cran.rstudio.com/")
+}
+
+# Install CRAN, GitHub and BioC packages with pak
+CRAN <- c('renv', 'data.table', 'jsonlite', 'httr', 'xml2', 'XML', 'ggplot2',
+    'ggtext', 'crayon', 'glue', 'RMySQL', 'RMariaDB', 'RSQLite', 'rextendr',
+    'renv', 'reticulate', 'caret', 'knitr', 'rmarkdown', 'tinytex',
+    'shiny', 'ggvis', 'htmlwidgets', 'plotly', 'xtable', 'foreach', 'parallel',
+    'future', 'devtools', 'roxygen2', 'testthat', 'drake', 'plumber',
+    'checkmate', 'profvis', 'bench', 'survival', 'lattice',
+    'RColorBrewer', 'ggplotify', 'gridExtra', 'qs', 'logger', 'memoise',
+    'languageserver'
+    )
+GITHUB <- c()
+BIOC <- c('BiocGenerics', 'MatrixGenerics', 'S4Vectors', 'Biobase',
+    'SummarizedExperiment', 'MultiAssayExperiment', 'GenomicRanges',
+    'GenomicFeatures', 'biomaRt', 'limma', 'AnnotationDbi', 'annotate',
+    'piano', 'lsa')
+
+pak::pkg_install(c(CRAN, GITHUB, BIOC), dependencies=TRUE)
