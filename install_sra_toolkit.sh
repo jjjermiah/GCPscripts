819ad4b99c1a4b110e8c505ae09766d3270b01e3
Jermiah Joseph <jermiahjoseph98@gmail.com>
2023-08-05 19:13:36 +0000

    changed from centos to ubuntu

--- install_sra_toolkit.sh
+++ install_sra_toolkit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-V=3.0.5
+V=3.0.6
 
 if [ "$UID" = "" ]
 then
@@ -20,7 +20,7 @@ apt-get --quiet install --yes libxml-libxml-perl
 echo "installing sra toolkit to /usr/local/ncbi"
 rm -rf .ncbi /usr/local/ncbi /etc/ncbi /etc/profile.d/sra-tools* # remove old install if any
 
-curl https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/$V/sratoolkit.$V-centos_linux64-cloud.tar.gz | tar xz -C /
+curl https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/$V/sratoolkit.$V-ubuntu64.tar.gz | tar xz -C /
 if [ $? -ne 0 ]
 then
     echo failure
@@ -28,3 +28,6 @@ then
 fi
 
 echo "Please 'source /etc/profile.d/sra-tools.sh' to setup your path"
+
+#RUN wget https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/${VERSION}/sratoolkit.${VERSION}-ubuntu64.tar.gz -O /tmp/sratoolkit.tar.gz \
+	# && tar zxvf /tmp/sratoolkit.tar.gz -C /opt/ && rm /tmp/sratoolkit.tar.gz 
